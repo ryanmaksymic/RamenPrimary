667acace32816ab2df738c497a056399ad540f39
ryanmaksymic-u9 <ryan.maksymic@unit9.com>
2023-02-24 13:06:34 -0500

    Implemented AddressablesManager

---
+++ Assets/Scripts/AddressablesManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public class AddressablesManager : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer playerSprite;
+    [SerializeField] private SpriteRenderer enemySprite;
+
+    AsyncOperationHandle<Sprite> opHandle;
+
+    public void Start()
+    {
+        StartCoroutine(LoadEnemySprite());
+    }
+
+    private IEnumerator LoadEnemySprite()
+    {
+        opHandle = Addressables.LoadAssetAsync<Sprite>("bowser");
+
+        yield return opHandle;
+
+        if (opHandle.Status == AsyncOperationStatus.Succeeded)
+        {
+            enemySprite.sprite = opHandle.Result;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Addressables.Release(opHandle);
+    }
+}
